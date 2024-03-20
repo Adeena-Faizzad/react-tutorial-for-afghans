179e39e7b3c5ab22d2881d2ce5a5a9981b4bfe9e
“Adeena-Faizzad” <130522128+Adeena-Faizzad@users.noreply.github.com>
2024-03-20 23:49:03 +0300

    NavLink
    Showing active link via NavLink

--- src/Components/RoutingFiles/Navbar.js
+++ src/Components/RoutingFiles/Navbar.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 function Navbar() {
   return (
-    <div>
-        <Link to='/'>Home</Link>
-        <Link to='/about'>About</Link>
-    </div>
+    <nav>
+        <NavLink to='/'>Home</NavLink>
+        <NavLink to='/about'>About</NavLink>
+    </nav>
   )
 }
 
